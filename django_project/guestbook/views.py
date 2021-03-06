5ddd24af5452f05c63a7dfbe915a877c0d70faed
Asman <iasman312@gmail.com>
2021-03-06 12:06:34 +0600

    создали вьюшку для домашней страницы

--- django_project/guestbook/views.py
+++ django_project/guestbook/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
-# Create your views here.
+from .models import Feedback
+from .forms import SearchForm
+
+
+def index_view(request):
+    form = SearchForm()
+    if request.GET.get('author'):
+        feedbacks = Feedback.objects.all().filter(
+            name__startswith=request.GET.get('author'))
+        return render(request, 'index.html', context={'feedbacks': feedbacks,
+                                                      'form': form})
+    feedbacks = Feedback.objects.all()
+    return render(request, 'index.html', context={'feedbacks': feedbacks,
+                                                  'form': form})
