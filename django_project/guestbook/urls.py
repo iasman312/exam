f9b7a03f75949ef0eb5b88dab3035a19a294e1ac
Asman <iasman312@gmail.com>
2021-03-06 13:49:38 +0600

    Создали вьюшку и шаблон для редактирования отзыва

--- django_project/guestbook/urls.py
+++ django_project/guestbook/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import (index_view,
-    feedback_create_view
+                    feedback_create_view,
+                    feedback_update_view
 )
 
 urlpatterns = [
     path('', index_view, name='feedback-list'),
     path('add/', feedback_create_view, name='product-add'),
+    path('<int:pk>/update', feedback_update_view, name='feedback-update')
 ]
--- django_project/guestbook/views.py
+++ django_project/guestbook/views.py
@@ -30,3 +30,28 @@ def feedback_create_view(request):
             feedback.save()
             return redirect('feedback-list')
         return render(request, 'feedback_create.html', context={'form': form})
+
+
+def feedback_update_view(request, pk):
+    feedback = get_object_or_404(Feedback, id=pk)
+
+    if request.method == 'GET':
+        form = FeedbackForm(initial={
+            'author': feedback.author,
+            'email': feedback.email,
+            'text': feedback.email
+        })
+        return render(request, 'feedback_update.html', context={'form': form,
+                                                                'feedback':
+                                                                    feedback})
+    elif request.method == 'POST':
+        form = FeedbackForm(data=request.POST)
+        if form.is_valid():
+            feedback.author = form.cleaned_data.get('author')
+            feedback.email = form.cleaned_data.get('email')
+            feedback.text = form.cleaned_data.get('text')
+            feedback.save()
+            return redirect('feedback-list')
+
+        return render(request, 'feedback_create.html',
+                      context={'form': form, 'feedback': feedback})
