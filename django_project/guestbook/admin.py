9060835999fcecde57a479236008079705641a92
Asman <iasman312@gmail.com>
2021-03-06 11:35:04 +0600

    Добавили модель в админскую часть

--- django_project/guestbook/admin.py
+++ django_project/guestbook/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Feedback
 
-# Register your models here.
+
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'status', 'email', 'created_at',
+                    'updated_at']
+    list_filter = ['author']
+    search_fields = ['author', 'status']
+    fields = ['author', 'email', 'text', 'status']
+    readonly_fields = ['id', 'created_at', 'updated_at']
+
+
+admin.site.register(Feedback, FeedbackAdmin)
